183f8bc554d675e7c5f84c8b3c1b96308e76e6d2
kavin <kavink.s.it.2020@snsct.org>
2024-02-22 19:24:35 +0530

    All frontend files added

---
+++ src/users/ViewUser.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+export default function ViewUser() {
+
+    const [user,setUsers]=useState({
+        name:"",
+        username:"",
+        email:""
+    });
+
+    const {id}=useParams();
+
+    useEffect(()=>{
+        loadUsers()
+    },[] );
+
+    const loadUsers=async ()=>{
+        const result=await axios.get(`http://localhost:8080/user/${id}`)
+        setUsers(result.data)
+    };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-5 shadow-lg">
+          <h2 className="text-center m-4">Employee Details</h2>
+
+          <div className="card">
+            <div className="card-header">
+              Details of Employee id : {user.id}
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">
+                  <b>Name:</b>
+                  {user.name}
+                </li>
+                <li className="list-group-item">
+                  <b>UserName:</b>
+                  {user.username}
+                </li>
+                <li className="list-group-item">
+                  <b>Email:</b>
+                  {user.email}
+                </li>
+              </ul>
+            </div>
+          </div>
+          <Link className="btn btn-success my-2" to={"/"}>
+            Back to Home
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
